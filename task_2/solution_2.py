8d14d7f82973ec899ce9c6b7326f6e53b9ca2d0d
Ksenia Dunaeva <71783164+zadushevno@users.noreply.github.com>
2020-10-19 20:29:49 +0500

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,8 @@
 # your code here
 user_input = input()
-# your code here
+most_frequent_character = user_input[0]
+print(most_frequent_character)
+for i in user_input:
+    if user_input.count(i) > user_input.count(most_frequent_character):
+        most_frequent_character = i
 print(most_frequent_character)
