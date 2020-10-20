d45d339f501ec86f5ce01d4781665bd9f14596b6
Ksenia Dunaeva <71783164+zadushevno@users.noreply.github.com>
2020-10-20 14:39:21 +0500

    Update solution_3_1.py

--- task_3/solution_3_1.py
+++ task_3/solution_3_1.py
@@ -1,4 +1,6 @@
-# your code here
-user_input = input()
-# your code here
+stroka = input()
+alphabet = ""
+for i in stroka:
+    if (i in alphabet) == False:
+        alphabet = alphabet + i
 print(alphabet)
