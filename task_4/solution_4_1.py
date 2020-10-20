f54241683397aecf7cf0173a68b9c693cad63db1
Ksenia Dunaeva <71783164+zadushevno@users.noreply.github.com>
2020-10-20 17:22:34 +0500

    Update solution_4_1.py

--- task_4/solution_4_1.py
+++ task_4/solution_4_1.py
@@ -1,6 +1,16 @@
-# your code here
-for smth in 'a'*10:
+for smth in 'a'*10: #мне кажется я где-то запуталась и вообще можно было решить проще :с
     user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+    if user_input == "":
+        pass
+    else:
+        if user_input[0] == '#':
+            pass
+        else:
+            f1 = user_input.find('\t')
+            f2 = user_input.find('\t', f1+1, len(user_input)-1)
+            form = user_input[f1+1:f2]
+            l1 = user_input.find('\t', f2+1, len(user_input)-1)
+            lemma = user_input[f2+1:l1]
+            my_cool_condition = lemma != form
+            if my_cool_condition == True:
+                print(form, lemma)
