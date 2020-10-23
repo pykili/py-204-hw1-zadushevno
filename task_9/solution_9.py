bc5efff958e973e3e9c16f886ede61089ddc905c
Ksenia Dunaeva <71783164+zadushevno@users.noreply.github.com>
2020-10-24 02:41:55 +0500

    Update solution_9.py

--- task_9/solution_9.py
+++ task_9/solution_9.py
@@ -1,1 +1,8 @@
-# your code here
+n = 100
+for i in range (1, n+1):
+    summ = 0
+    for j in range (1, 2*i):
+        if j%2 != 0:
+            summ = summ + j
+    if summ == i*i:
+        print(i, ': true')
