394c9dd006ce015d8d8f97e2841ce9cfefc682dd
Ksenia Dunaeva <71783164+zadushevno@users.noreply.github.com>
2020-10-26 16:32:20 +0500

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,8 +1,20 @@
 # your code here
 user_input = input()
 most_frequent_character = user_input[0]
-print(most_frequent_character)
+frequency = 0
+for smth in user_input:
+    if smth == most_frequent_character:
+        frequency = frequency + 1      
 for i in user_input:
-    if user_input.count(i) > user_input.count(most_frequent_character):
+    count = 0
+    frequency = 0
+    symbol = i
+    for smth in user_input:
+        if smth == symbol:
+            count = count + 1
+    for smth in user_input:
+        if smth == most_frequent_character:
+            frequency = frequency + 1
+    if count > frequency:
         most_frequent_character = i
 print(most_frequent_character)
