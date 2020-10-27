e239cacb59d54bbecefd8161d08da94ecac525f2
Ksenia Dunaeva <71783164+zadushevno@users.noreply.github.com>
2020-10-28 02:43:24 +0500

    Update solution_4_2.py

--- task_4/solution_4_2.py
+++ task_4/solution_4_2.py
@@ -1,6 +1,43 @@
-# your code here
-for smth in 'a'*10:
+for smth in 'a'*10: #мне кажется я где-то запуталась и вообще можно было решить проще :с
     user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+    form = ""
+    lemma = ""
+    pos_t1 = 0
+    pos_t2 = 0
+    pos_t3 = 0
+    count = 0
+    if user_input == "":
+        pass
+    else:
+        if user_input[0] == '#':
+            pass
+        else:
+            for i in user_input:
+                if i == '\t':
+                    if pos_t1 == 0:
+                        pos_t1 = count
+                    else:
+                        if pos_t2 == 0:
+                            pos_t2 = count
+                        else:
+                            if pos_t3 == 0: 
+                                pos_t3 = count
+                count = count + 1
+            symbol_pos = 0
+            for j in user_input:
+                if symbol_pos > pos_t1 and symbol_pos < pos_t2:
+                    form = form + j
+                if symbol_pos > pos_t2 and symbol_pos < pos_t3:
+                    lemma = lemma + j
+                symbol_pos = symbol_pos + 1
+            symbol_pos = 0
+            lemma_len = 0 
+            for a in lemma:
+                lemma_len = lemma_len + 1
+            form_beginning = ""
+            for i in form:
+                if symbol_pos < lemma_len:
+                    form_beginning = form_beginning + i
+                symbol_pos = symbol_pos + 1
+            if lemma != form_beginning:
+                print(form, lemma) 
