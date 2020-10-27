dc9e4b5e7bf2fad9d6c2198b0d9d5bc460bbee3a
Ksenia Dunaeva <71783164+zadushevno@users.noreply.github.com>
2020-10-28 02:28:32 +0500

    Update solution_4_1.py

--- task_4/solution_4_1.py
+++ task_4/solution_4_1.py
@@ -1,16 +1,35 @@
-for smth in 'a'*10: #мне кажется я где-то запуталась и вообще можно было решить проще :с
+for smth in 'a'*10:
     user_input = input()
+    form = ""
+    lemma = ""
+    pos_t1 = 0
+    pos_t2 = 0
+    pos_t3 = 0
+    count = 0
     if user_input == "":
         pass
     else:
         if user_input[0] == '#':
             pass
         else:
-            f1 = user_input.find('\t')
-            f2 = user_input.find('\t', f1+1, len(user_input)-1)
-            form = user_input[f1+1:f2]
-            l1 = user_input.find('\t', f2+1, len(user_input)-1)
-            lemma = user_input[f2+1:l1]
+            for i in user_input:
+                if i == '\t':
+                    if pos_t1 == 0:
+                        pos_t1 = count
+                    else:
+                        if pos_t2 == 0:
+                            pos_t2 = count
+                        else:
+                            if pos_t3 == 0: 
+                                pos_t3 = count
+                count = count + 1
+            symbol_pos = 0
+            for j in user_input:
+                if symbol_pos > pos_t1 and symbol_pos < pos_t2:
+                    form = form + j
+                if symbol_pos > pos_t2 and symbol_pos < pos_t3:
+                    lemma = lemma + j
+                symbol_pos = symbol_pos + 1
             my_cool_condition = lemma != form
             if my_cool_condition == True:
-                print(form, lemma)
+                print(form, lemma) 
